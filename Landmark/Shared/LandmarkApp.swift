2ee786d618d5fac8abd37fe644612dd2c041ec26
Toni Kari <42929889+TToniK@users.noreply.github.com>
2021-01-11 10:22:28 +0200

    Project started

---
+++ Landmark/Shared/LandmarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarkApp.swift
+//  Shared
+//
+//  Created by Toni Kari on 11/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct LandmarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
