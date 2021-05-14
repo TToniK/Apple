be7f4c8d80c3c4857ba07c0da4a89f516c1ab911
Toni Kari <42929889+TToniK@users.noreply.github.com>
2021-05-14 15:21:33 +0300

    ToDoList
    ToDoList is working

---
+++ ToDoList/Shared/ToDoListApp.swift
@@ -0,0 +1,20 @@
+//
+//  ToDoListApp.swift
+//  Shared
+//
+//  Created by Toni Kari on 3.5.2021.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    @StateObject var order = Order()
+
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+                .environmentObject(order)
+        }
+    }
+}
